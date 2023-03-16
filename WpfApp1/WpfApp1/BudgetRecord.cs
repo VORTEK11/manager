2bb24b5511d2be0f164dfecfe5565cb6a6c63178
Vortek <113912297+VORTEK11@users.noreply.github.com>
2023-03-16 22:34:39 +0300

    Я есть индус

---
+++ WpfApp1/WpfApp1/BudgetRecord.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+namespace WpfApp1
+{
+    [Serializable]
+    public class BudgetRecord
+    {
+        private string _name;
+        private RecordType _type;
+        private decimal _amount;
+        private bool _isDeduction;
+        private DateTime _date;
+
+        public BudgetRecord(string name, RecordType type, decimal amount, bool isDeduction, DateTime date)
+        {
+            _name = name;
+            _type = type;
+            _amount = amount;
+            _isDeduction = isDeduction;
+            _date = date;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public RecordType Type
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public decimal Amount
+        {
+            get
+            {
+                return _amount;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _isDeduction = true;
+                    _amount = Math.Abs(value);
+                }
+                else
+                {
+                    _isDeduction = false;
+                    _amount = value;
+                }
+            }
+        }
+
+        public bool IsDeduction
+        {
+            get
+            {
+                return _isDeduction;
+            }
+            set
+            {
+                _isDeduction = value;
+            }
+        }
+
+        public DateTime Date
+        {
+            get
+            {
+                return _date;
+            }
+            set
+            {
+                _date = value;
+            }
+        }
+    }
+
+    public enum RecordType
+    {
+        Income,
+        Expense
+    }
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,130 @@
+﻿
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows;
+using WpfApp1;
+
+namespace BudgetManager
+{
+    [Serializable]
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private ObservableCollection<BudgetRecord> _records;
+        private string _filePath = "records.dat";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadData();
+            DataContext = this;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<BudgetRecord> Records
+        {
+            get
+            {
+                return _records;
+            }
+            set
+            {
+                _records = value;
+                OnPropertyChanged("Records");
+            }
+        }
+
+        public DateTime Today
+        {
+            get
+            {
+                return DateTime.Today;
+            }
+        }
+
+        public decimal Total
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (BudgetRecord record in _records)
+                {
+                    total += record.Amount;
+                }
+
+                return total;
+            }
+        }
+
+        private void LoadData()
+        {
+            if (File.Exists(_filePath))
+            {
+                using (Stream stream = File.Open(_filePath, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    Records = (ObservableCollection<BudgetRecord>)formatter.Deserialize(stream);
+                }
+            }
+            else
+            {
+                Records = new ObservableCollection<BudgetRecord>();
+            }
+        }
+
+        private void SaveData()
+        {
+            using (Stream stream = File.Open(_filePath, FileMode.Create))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                formatter.Serialize(stream, _records);
+            }
+        }
+
+      /*  private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            AddRecordWindow window = new AddRecordWindow();
+            window.Owner = this;
+            if (window.ShowDialog() == true)
+            {
+                Records.Add(window.Record);
+            }
+        }*/
+
+      /*  private void EditButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                EditRecordWindow window = new EditRecordWindow((BudgetRecord)dataGrid.SelectedItem);  
+                window.Owner = this;
+                if (window.ShowDialog() == true)
+                {
+                    int index = Records.IndexOf((BudgetRecord)dataGrid.SelectedItem);
+                    Records.RemoveAt(index);
+                    Records.Insert(index, window.Record);
+                }
+            }
+        }*/
+
+        private void DeleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedItem != null)
+            {
+                Records.Remove((BudgetRecord)dataGrid.SelectedItem);
+            }
+        }
+
+        private void Window_Closing(object sender, CancelEventArgs e)
+        {
+            SaveData();
+        }
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
